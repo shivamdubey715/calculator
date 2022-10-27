3701750f9e12571723f5bee126db5fca689d0f57
Shivamkumar <shivammessi07@gmail.com>
2022-10-27 11:21:06 +0530

    other operators added

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
-var buttons = document.getElementsByClassName('button');
-var display = document.getElementById('display');
+var buttons = document.getElementsByClassName("button");
+var display = document.getElementById("display-text");
 
 var t1 = 0;
 var t2 = null;
 var operator = null;
 
 function isOperator(value) {
-    if (value == '+' || value == '*' || value == '/' || value == '-') {
-        return true;
-    }
-    return false;
+    return value == "+" || value == "-" || value == "*" || value == "/";
 }
 
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].setAttribute('click', function () {
+    buttons[i].addEventListener('click', function () {
 
         var value = this.getAttribute('data-value');
         var text = display.textContent.trim();
         if (isOperator(value)) {
             operator = value;
-            t1.parseFloat(text);
-            display.textContent = "";
-        }
-        else if (value == "ac") {
-            display.textContent = "";
-        }
-        else if (value == "sign") {
             t1 = parseFloat(text);
+            display.textContent = "";
+        }
+        else if(value == 'ac'){
+            display.textContent = "";
+        }
+        else if (value == "sign"){
+            operator = parseFloat(text);
+            operator = operator * (-1);
+            display.textContent = operator;
+        }
+        else if(value == "."){
+            if(text.length && !text.includes('.')){
+                display.textContent = text + '.';
+            }
+        }
+        else if(value == '%'){
+            t1 = parseFloat(text);
+            t1 = t1/100;
+            display.textContent = t1;
+        }
+        else if (value == "=") {
+            t2 = parseFloat(text);
+            var result = eval(t1 + ' ' + operator + ' ' + t2);
+            if (result) {
+                display.textContent = result;
+                t1 = result;
+                t2 = null;
+                operator = null;
+            }
+        } 
+        else {
+            display.textContent += value;
         }
 
     });
